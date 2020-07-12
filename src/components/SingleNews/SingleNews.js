e86d4cd85bd72c8cd8bd1e909b40dbed804f5765
Mikhail Shvedov <micangel@mail.ru>
2020-07-12 23:04:46 +0300

    Add the link to url

--- src/components/SingleNews/SingleNews.js
+++ src/components/SingleNews/SingleNews.js
@@ -8,25 +8,30 @@ export default function (props) {
         description,
         urlToImage,
         source,
+        url
     } = props
 
     return (
         <div className={'container'}>
             <div className={'row'}>
                 <div className={'col col-md-4'}>
-                    <h1 className={'singleNews__title'}>{title}</h1>
-                    <div className={'singleNews__source'}>
-                        {source.name}
+                    <div className={'singleNews'}>
+                        <h1 className={'singleNews__title'}>{title}</h1>
+                        <a href={url} className={'singleNews__source'} target={'_blank'}>
+                            {source.name}
+                        </a>
+
+                        <Date {...props}/>
                     </div>
-
-                    <Date {...props}/>
-
                 </div>
                 <div className={'col col-md-8'}>
                     <div className={'image'}>
                         <img src={urlToImage}></img>
                     </div>
                     <p className={'singleNews__content'}>{description}</p>
+                    <a href={url} className={'singleNews__link'} target={'_blank'}>
+                        Продолжение ...
+                    </a>
                 </div>
             </div>
         </div>
